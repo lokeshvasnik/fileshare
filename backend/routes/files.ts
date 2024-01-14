93ff74b15288956e91a004c696e3d4b1fe33fab4
lokeshvasnik <lokeshvasnik2003@gmail.com>
2024-01-14 17:58:08 +0530

    fix backend download bug

--- backend/routes/files.ts
+++ backend/routes/files.ts
@@ -2,6 +2,8 @@ import express from "express";
 import multer from "multer";
 import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
 import File from "../models/File";
+import mongoose from "mongoose";
+import https from "https";
 
 const router = express.Router();
 const storage = multer.diskStorage({});
@@ -48,6 +50,52 @@ router.post("/upload", upload.single("myFile"), async (req, res) => {
         console.log(error);
 
         return res.status(500).json({ messages: "Server Error :(" });
+    }
+});
+
+//? @gets file
+
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid ID format" });
+    }
+    const file = await File.findById(id);
+
+    try {
+        if (!file) {
+            return res.status(404).json({ messsage: "Not Exist" });
+        }
+        const { filename, format, sizeInBytes } = file;
+        return res.status(200).json({
+            name: filename,
+            sizeInBytes,
+            format,
+            id,
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "Server Error" });
+    }
+});
+
+//? #downloads file
+router.get("/:id/download", async (req, res) => {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid ID format" });
+    }
+
+    try {
+        const file = await File.findById(id);
+
+        if (!file) {
+            return res.status(404).json({ message: "Not Exist" });
+        }
+
+        // Use optional chaining to check if file and secure_url are not undefined
+        https.get(file?.secure_url, (fileStream) => fileStream.pipe(res));
+    } catch (error) {
+        return res.status(500).json({ message: "Server Error" });
     }
 });
 
